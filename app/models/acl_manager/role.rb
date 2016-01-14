5d7c83605e029ba1322c02424baaa8d6a67a9a1e
Gil <gil.gomes@flexait.com.br>
2016-01-14 11:40:30 -0200

    Removes method from flexacloud

--- app/models/acl_manager/role.rb
+++ app/models/acl_manager/role.rb
@@ -14,14 +14,5 @@ module AclManager
     def included? acl
       self.acl_ids.include?(acl.id)
     end
-
-    def self.admin
-      role = Role.find_or_create_by(name: 'admin', active: true)
-      if role.acls.empty?
-        role.acls << Acl.find_by(name: 'app', namespace: 'app')
-        role.acls << Acl.find_by(name: 'devise', namespace: 'devise')
-      end
-      role
-    end
   end
 end
--- lib/acl_manager/engine.rb
+++ lib/acl_manager/engine.rb
@@ -2,10 +2,5 @@ module AclManager
   class Engine < ::Rails::Engine
     isolate_namespace AclManager
 
-    initializer 'acl_manager.action_controller' do |app|
-      ActiveSupport.on_load :action_controller do
-        helper AclManager::AclsHelper
-      end
-    end
   end
 end
