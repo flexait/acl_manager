ed4e300799195b8e29ba328346fd98260fa43548
Gil Gomes <gilgomesp@gmail.com>
2016-03-29 14:42:20 -0300

    Importing acl migrations to main app

--- lib/acl_manager/engine.rb
+++ lib/acl_manager/engine.rb
@@ -5,6 +5,11 @@ end
 module AclManager
   class Engine < ::Rails::Engine
     isolate_namespace AclManager
-
-  end
+    initializer :append_migrations do |app|
+		unless app.root.to_s.match(root.to_s)
+			config.paths["db/migrate"].expanded.each do |p|
+				app.config.paths["db/migrate"] << p
+			end
+		end
+	end
 end
