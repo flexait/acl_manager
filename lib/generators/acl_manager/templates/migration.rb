fafc862d546b12fafe56b09d477b37345db9f597
Denilson <ddts80@gmail.com>
2016-08-10 09:52:11 -0300

    Migration template

--- lib/generators/acl_manager/templates/migration.rb
+++ lib/generators/acl_manager/templates/migration.rb
@@ -8,11 +8,10 @@ class AddAclManagerTo<%= table_name.camelize %> < ActiveRecord::Migration
       t.timestamps null: false
     end
 
-    create_table :acl_manager_acls_roles do |t|
-      t.references :acl_manager_acl, index: true
-      t.references :acl_manager_role, index: true
+    create_join_table :acl_manager_acls, :acl_manager_roles, table_name: 'acl_manager_acls_roles' do |t|
+      t.index [:acl_manager_acl_id, :acl_manager_role_id]
+      t.index [:acl_manager_role_id, :acl_manager_acl_id]
     end
-
 
     create_table :acl_manager_acls do |t|
       t.string :name
@@ -29,16 +28,9 @@ class AddAclManagerTo<%= table_name.camelize %> < ActiveRecord::Migration
       t.timestamps null: false
     end
 
-    create_table :acl_manager_roles_<%= table_name %> do |t|
-      t.references :user, index: true
-      t.references :acl_manager_role, index: true
-
-      t.timestamps null: false
+    create_join_table :acl_manager_roles, :<%= table_name %> do |t|
+      t.index [:acl_manager_role_id, :<%= table_name.singularize %>_id]
+      t.index [:<%= table_name.singularize %>_id, :acl_manager_role_id]
     end
-
-    add_foreign_key :acl_manager_acls_roles, :acl_manager_acls
-    add_foreign_key :acl_manager_acls_roles, :acl_manager_roles
-    add_foreign_key :acl_manager_roles_<%= table_name %>, :<%= table_name %>
-    add_foreign_key :acl_manager_roles_<%= table_name %>, :acl_manager_roles
   end
 end
