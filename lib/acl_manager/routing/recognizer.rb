630a69cb23f18cd52d614a37ffdaea7f63f567f1
Gil Gomes <gilgomesp@gmail.com>
2018-05-23 22:05:15 -0300

    splitting method into small ones

--- lib/acl_manager/routing/recognizer.rb
+++ lib/acl_manager/routing/recognizer.rb
@@ -3,11 +3,19 @@ require 'action_dispatch/routing/inspector'
 module AclManager
   module Routing
     class Recognizer
-      def self.fullpath url_path, options
-        raise 'Bad link_to usage. You don\'t need href with no URL. Remove it.' if url_path.include?('javascript:')
+      def self.fullpath(url_path, options)
+        new(url_path, options).fullpath
+      end
 
-        route = find(url_path, options)
-        Rails.logger.debug "[Route Extractor] #{route}"
+      def initialize(url_path, options)
+        @url_path = url_path
+        @options  = options
+      end
+
+      def fullṕath
+        raise 'Bad link_to usage. You don\'t need href with no URL. Remove it.' if @url_path.include?('javascript:')
+
+        route = find(@url_path, @options)
         path = route[:controller].include?('/') && !route[:controller].include?('rails/') ? route[:controller].split('/') : ['none', route[:controller].gsub('rails/', '')]
 
         {namespace: path.first, controller: path.last, action: route[:action]}
@@ -15,34 +23,39 @@ module AclManager
 
       private
 
-      def self.find(path, options)
+      def find
         begin
-          Rails.logger.debug "[Route Extractor] Start #{path}"
-          recognized_path = Rails.application.routes.recognize_path(path, options)
+          recognized_path = find_on_rails_app
         rescue ActionController::RoutingError
           Rails::Engine.subclasses.each do |engine|
             begin
-              engine_name = engine.name.split("::").first.underscore
-              Rails.logger.debug "[Route Extractor] Try for engine #{engine_name}"
-
-              route = all.find{ |r| r[:namespace] == engine_name }
-              next if route.nil?
-              engine_path = path.gsub(route[:path], '')
-
-              if(engine_path.match(/#{engine_name}(\/.*)/))
-                recognized_path = engine.routes.recognize_path($1, options)
-              end
+              recognized_path = find_on_engine(engine)
             rescue ActionController::RoutingError
-              Rails.logger.debug "[Route Extractor][Error] No route found for #{path}"
+              next
             end
           end
         end
-        Rails.logger.debug "[Route Extractor] Route found: #{recognized_path}"
-        recognized_path.blank? ? {controller: path} : recognized_path
+
+        recognized_path.blank? ? {controller: @url_path} : recognized_path
       end
 
-      def self.all
-        @@all ||= RouteExtractor.new.all[:routes]
+      def find_on_engine(engine)
+        engine.routes.recognize_path($1, @options) if valid_route?
+      end
+
+      def valid_route?
+        engine_name = engine.name.split("::").first.underscore
+        route = all.find{ |r| r[:namespace] == engine_name }
+
+        route.present? && @url_path.gsub(route[:path], '').match(/#{engine_name}(\/.*)/)
+      end
+
+      def find_on_rails_app
+        Rails.application.routes.recognize_path(@url_path, @options)
+      end
+
+      def all
+        @@all ||= AclManager::RouteExtractor.new.all[:routes]
       end
     end
   end
