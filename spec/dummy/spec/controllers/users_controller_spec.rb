4e7a321785228bf380617cd9c61c1ee99725601d
Denilson <ddts80@gmail.com>
2018-06-07 16:47:06 -0300

    Tests for users controller

--- spec/dummy/spec/controllers/users_controller_spec.rb
+++ spec/dummy/spec/controllers/users_controller_spec.rb
@@ -29,11 +29,16 @@ RSpec.describe UsersController, type: :controller do
   # User. As you add validations to User, be sure to
   # adjust the attributes here as well.
   let(:valid_attributes) {
-    skip("Add a hash of attributes valid for your model")
+    {
+      email: 'john@email.com',
+      password: '123456789'
+    }
   }
 
   let(:invalid_attributes) {
-    skip("Add a hash of attributes invalid for your model")
+    {
+      email: 'john'
+    }
   }
 
   # This should return the minimal set of values that should be in the session
@@ -97,14 +102,19 @@ RSpec.describe UsersController, type: :controller do
   describe "PUT #update" do
     context "with valid params" do
       let(:new_attributes) {
-        skip("Add a hash of attributes valid for your model")
+        {
+          email: 'anne@email.com',
+          password: '123456789'
+        }
       }
 
       it "updates the requested user" do
         user = User.create! valid_attributes
+        expect(user.email).to eq('john@email.com')
+
         put :update, params: {id: user.to_param, user: new_attributes}, session: valid_session
         user.reload
-        skip("Add assertions for updated state")
+        expect(user.email).to eq('anne@email.com')
       end
 
       it "redirects to the user" do
