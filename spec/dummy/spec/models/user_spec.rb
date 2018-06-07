15c996f076d3ab4e3882fa4113eca0951224cb6e
Denilson <ddts80@gmail.com>
2018-06-07 16:37:51 -0300

    Test user model

--- spec/dummy/spec/models/user_spec.rb
+++ spec/dummy/spec/models/user_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:valid_parameters) {
+    {
+      email: 'john@email.com',
+      password: '123456789'
+    }
+  }
+  
+  describe '#create!' do
+    context 'with invalid parameter' do
+      it 'email' do
+        expect {
+          User.create!(email: 'john')
+        }.to raise_error(ActiveRecord::RecordInvalid)
+      end
+    end
+
+    context 'with valid parameters' do
+      it 'should be success' do
+        user = User.create!(valid_parameters)
+        expect(user).to eq(User.first)
+      end
+    end
+  end
 end
